d9e4879f763752bbd3267050f99b3446af7b20ac
GuPLF0 <gpedr56@gmail.com>
2023-01-12 15:22:16 -0300

    Fixing ProductController

--- src/main/java/com/gusdev/demo/controllers/ProductController.java
+++ src/main/java/com/gusdev/demo/controllers/ProductController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/categories")
+@RequestMapping(value = "/products")
 public class ProductController {
 
     private ProductService service;
