aec9b1f8bb1b0728355a94ed65f02338060b70eb
GuPLF0 <gpedr56@gmail.com>
2023-01-15 15:09:06 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/gusdev/demo/models/Category.java
+++ src/main/java/com/gusdev/demo/models/Category.java
@@ -1,5 +1,6 @@
 package com.gusdev.demo.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,8 @@ public class Category implements Serializable {
     private Integer id;
     private String name;
 
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category() {
